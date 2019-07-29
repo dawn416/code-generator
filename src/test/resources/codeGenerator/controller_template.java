2dacb34ce58e2672d0f8db167e9d3fb088694910
黎明 <1141905101@qq.com>
2019-07-29 17:39:21 +0800

    测试配置文件

---
+++ src/test/resources/codeGenerator/controller_template.java
@@ -0,0 +1,16 @@
+﻿package ${templateItem.controller_template.targetPackage};
+
+//import org.springframework.stereotype.Controller;
+
+import ${templateItem.entity_template.targetPackage}.${tableItem.beanName};
+<#list tableItem.columnList as pk>
+<#if pk.id>
+    <#assign primaryKey=pk>
+<#break>
+</#if>
+</#list>
+ 
+//@Controller
+public class ${tableItem.beanName}Controller {
+
+}
---
+++ src/test/resources/codeGenerator/service_template.java
@@ -0,0 +1,17 @@
+﻿package ${templateItem.service_template.targetPackage};
+
+import ${templateItem.entity_template.targetPackage}.${tableItem.beanName};
+<#list tableItem.columnList as pk>
+<#if pk.id>
+    <#assign primaryKey=pk>
+<#break>
+</#if>
+</#list>
+
+public interface ${tableItem.beanName}Service {
+    int deleteByPrimaryKey(${primaryKey.javaType} ${primaryKey.fieldName});
+    
+    int updateByPrimaryKey(${tableItem.beanName} ${tableItem.beanName?uncap_first});
+    
+    int insert(${tableItem.beanName} ${tableItem.beanName?uncap_first});
+}
